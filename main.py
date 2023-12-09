d1c2c97ef65ba439c190ed2085dca58b7b13d766
Nitin Joshi <123286449+NitinJoshi10@users.noreply.github.com>
2023-12-09 16:01:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from secret import password
 def print_hi(name):
     print(f"Hi, {name}")
 
-print_hi("Nitin")
+print_hi("Write_your_name_here")
 
 def my_fun(a, b):
     return a - b
 
 if __name__ == "__main__":
-    print("Atul")
+    print("Your_name")
     print(my_fun(5, 1))
 
 print(password)
